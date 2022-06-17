6eee9d47abcc558d2be8666886ca5682fbfe9bd8
nelson023 <84180654+nelson023@users.noreply.github.com>
2022-06-17 17:44:39 +0800

    Update 5-5.js

--- HW/JavaScript/hw5/5-5.js
+++ HW/JavaScript/hw5/5-5.js
@@ -1,1 +1,12 @@
-
+function Avg(Array)
+{
+    var i = 0, sum = 0, ArrayLen = Array.length;
+    while (i < ArrayLen)
+    {
+        sum = sum + Array[i++];
+}
+    return sum / ArrayLen;
+}
+var Array = [1, 5, 2, 3, 7];
+var a = Avg(Array);
+console.log(a)
