9e5c05591250174dd789c6ce44b9b17b5ff370f9
nelson023 <84180654+nelson023@users.noreply.github.com>
2022-06-17 18:36:06 +0800

    Update 5-7.js

--- HW/JavaScript/hw5/5-7.js
+++ HW/JavaScript/hw5/5-7.js
@@ -1,2 +1,9 @@
-
-
+let a=([[1,2,3], [3,2,1]]);
+let b=[];
+for(var i = 0 ; i < a[0].length ; i++){
+    b[i]=[];
+    for(var j = 0 ; j < a.length ; j++){
+        b[i][j] = a[j][i];
+    }
+}
+console.log(a);
