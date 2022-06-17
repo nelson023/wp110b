e7c12e8363f8599434ec51616927e4ba6a792b17
nelson023 <84180654+nelson023@users.noreply.github.com>
2022-06-17 21:17:37 +0800

    Update 5-9.js

--- HW/JavaScript/hw5/5-9.js
+++ HW/JavaScript/hw5/5-9.js
@@ -1,1 +1,15 @@
+不會所以找網路
 
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib(10)
+// 55
+fib(6)
+// 8
+
+https://jimmyswebnote.com/javascript-fibonacci-numbers/
